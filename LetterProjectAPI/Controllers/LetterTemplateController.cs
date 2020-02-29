92ceb71101faa127d64753856eac76c8f662f30f
ricky-bruner <alteredecho07@yahoo.com>
2020-02-29 10:15:38 -0600

    adding sln and initial API

---
+++ LetterProjectAPI/Controllers/LetterTemplateController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Utilities;
+
+namespace LetterProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LetterTemplateController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public LetterTemplateController(IConfiguration config) 
+        {
+            _config = config;
+        }
+
+        public IMongoCollection<BsonDocument> Connection
+        {
+            get
+            {
+                MongoClient client = new MongoClient(Config.GetMongoConnectionString());
+                IMongoDatabase database = client.GetDatabase(Config.GetDatabase());
+
+                return database.GetCollection<BsonDocument>(Config.GetCollectionName("Templates"));
+            }
+        }
+
+        // GET api/lettertemplates?q=name
+        [HttpGet("{name:length(24)}", Name = "GetTemplate")]
+        public BsonDocument Get(string q)
+        {
+            string query = "{ 'name': '" + q + "'}";
+
+            BsonDocument filter = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(query);
+            ProjectionDefinition<BsonDocument> projectionDefinition = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>("{}");
+            return Connection.Find(filter).Project(projectionDefinition).FirstOrDefault();
+
+        }
+
+
+        [HttpPost]
+        public ActionResult<BsonDocument> Create(BsonDocument template)
+        {
+
+            BsonDocument filter = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(template);
+            Connection.InsertOne(filter);
+
+            return CreatedAtRoute("GetTemplate", new { name = template["name"].ToString() }, template);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public ActionResult Update(string id, BsonDocument templateIn)
+        {
+            var template = Get(id);
+
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            BsonDocument filter = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(template);
+            Connection.ReplaceOne(filter, templateIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public ActionResult Delete(string id)
+        {
+            var template = Get(id);
+
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            BsonDocument filter = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(template);
+            Connection.DeleteOne(filter);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ LetterProjectAPI/Utilities/Config.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace Utilities
+{
+    public static class Config
+    {
+        public static List<string> GetKeys(string fileName)
+        {
+            List<string> keys = new List<string>();
+
+            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(fileName);
+            IConfigurationRoot configuration = builder.Build();
+            IEnumerable<IConfigurationSection> configSections = configuration.GetChildren().ToList();
+
+            foreach (IConfigurationSection section in configSections)
+            {
+                keys.Add(section.Key);
+            }
+
+            return keys;
+        }
+
+        public static string GetString(string fileName, string sectionPath)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(fileName);
+            IConfigurationRoot configuration = builder.Build();
+            IConfigurationSection configSection = configuration.GetSection(sectionPath);
+
+            return configSection.Value;
+        }
+
+        public static List<string> GetListOfStrings(string fileName, string sectionPath)
+        {
+            List<string> list = new List<string>();
+
+            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(fileName);
+            IConfigurationRoot configuration = builder.Build();
+            IConfigurationSection configSection = configuration.GetSection(sectionPath);
+
+            foreach (IConfigurationSection section in configSection.GetChildren())
+            {
+                list.Add(section.Value);
+            }
+
+            return list;
+        }
+
+        public static IConfigurationSection GetObjectFromArray(string fileName, string arrayPath, string objectName)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(fileName);
+            IConfigurationRoot configuration = builder.Build();
+            IConfigurationSection configSection = configuration.GetSection(arrayPath);
+            IConfigurationSection objectValue = configSection.GetChildren().Where(c => c["name"] == objectName).FirstOrDefault();
+
+            return objectValue;
+        }
+
+        public static List<IConfigurationSection> GetArrayOfObjects(string fileName, string arrayPath)
+        {
+            List<IConfigurationSection> list = new List<IConfigurationSection>();
+
+            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(fileName);
+            IConfigurationRoot configuration = builder.Build();
+            IConfigurationSection configSection = configuration.GetSection(arrayPath);
+
+            foreach (IConfigurationSection section in configSection.GetChildren())
+            {
+                list.Add(section);
+            }
+
+            return list;
+        }
+
+        public static string GetStringFromObject(string fileName, string objectPath, string name)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(fileName);
+            IConfigurationRoot configuration = builder.Build();
+            IConfigurationSection configSection = configuration.GetSection(objectPath);
+
+            return configSection[name];
+        }
+
+        public static string GetMongoConnectionString()
+        {
+            return GetString("config.json", "MongoConnectionString");
+        }
+
+        public static string GetDatabase()
+        {
+            bool isOpenSSL = GetIsOpenSSL();
+
+            if (isOpenSSL)
+            {
+                string databaseName = GetString("config.json", "DatabaseName");
+
+                return databaseName;
+            }
+            else
+            {
+                string mongoConnection = GetMongoConnectionString();
+                string[] splits = mongoConnection.Split('/');
+
+                return splits[splits.Length - 1];
+            }
+        }
+
+        public static string GetCollectionName(string collection)
+        {
+            return GetStringFromObject("config.json", "DatabaseCollections", collection);
+
+        }
+
+        public static bool GetIsOpenSSL()
+        {
+            string value = GetString("config.json", "IsOpenSSL");
+
+            if (string.IsNullOrEmpty(value))
+            {
+                return false;
+            }
+
+            if (value.ToLower() == "false")
+            {
+                return false;
+            }
+            else if (value.ToLower() == "true")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
